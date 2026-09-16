e27830bfd8528830d94a3feb3a5b20b03fd1c082
Albert <albert.giro@gmail.com>
2026-09-16 10:46:40 +0200

    chore: prepare next development iteration (0.1.2-SNAPSHOT)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.agiro.fanel.android"
         minSdk = 29
         targetSdk = 36
-        versionCode = 101
-        versionName = "0.1.1"
+        versionCode = 102
+        versionName = "0.1.2-SNAPSHOT"
 
         buildConfigField("String", "API_BASE_URL", "\"http://10.0.2.2:8080/\"")
     }
